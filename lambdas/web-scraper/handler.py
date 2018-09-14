44a4b1c1947d2ad4787e494aca84d3254883de67
Drew <ATDaniel422@gmail.com>
2018-09-13 23:09:31 -0400

    Got Polly API call working. Now just need to partition text for multiple api calls.

--- lambdas/web-scraper/handler.py
+++ lambdas/web-scraper/handler.py
@@ -1,8 +1,8 @@
 import json
 import requests
 import bs4
-import selenium
-
+import boto3
+from contextlib import closing
 
 def web_scraper(event, context):
     body = {
@@ -15,15 +15,29 @@ def web_scraper(event, context):
     response = requests.get(url)
 
     # convert the source html to a beautifulSoup object, select all <p> tags
-    website_full_text = bs4.BeautifulSoup(response.text)
-    website_raw_text = website_full_text.select('p')
+    website_full_text = bs4.BeautifulSoup(response.text, features="lxml")
+    # website_raw_text = website_full_text.select('p')
+    website_raw_text = website_full_text.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
 
     # Take the list of <p> tags and get the nested text as strings
     website_text = []
     for i in range(len(website_raw_text)):
         website_text.append(website_raw_text[i].getText())
-        print(website_text[i])
+#        print(website_text[i])
 
+    # Amazon Polly API Call
+    polly = boto3.client("polly")
+    audio_response = polly.synthesize_speech(
+        Text = "The cake is a lie. The cake is a Lie. Hello Dave, I can't let you do that Dave.",
+        TextType = "text",
+        OutputFormat = "mp3",
+        VoiceId="Joanna"
+    )
+    
+    print(audio_response)
+    with closing(audio_response['AudioStream']) as stream:
+        with open('audio_file.mp3', 'wb') as audio_file:
+            audio_file.write(stream.read())
 
     response = {
         "statusCode": 200,
