d9d8de21c5a64665c1c03d392e64cb6b747d3f04
bransynluther <bransynluther@gmail.com>
2018-09-18 12:53:44 -0400

    Adds the funtionality of prefixing file names

--- lambdas/web-scraper/handler.py
+++ lambdas/web-scraper/handler.py
@@ -5,13 +5,14 @@ import boto3
 from contextlib import closing
 
 def web_scraper(event, context):
-    
+
     # Grab the url from the event json and grab the source file using requests
     url = event['url_search']
+    file_name = event['file_name']
     response = requests.get(url)
 
     # convert the source html to a beautifulSoup object, select all <p> tags
-    website_full_text = bs4.BeautifulSoup(response.text, features="lxml")
+    website_full_text = bs4.BeautifulSoup(response.text, features="html5lib")
     # website_raw_text = website_full_text.select('p')
     website_raw_text = website_full_text.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
 
@@ -19,7 +20,7 @@ def web_scraper(event, context):
     website_text = []
     for i in range(len(website_raw_text)):
         website_text.append(website_raw_text[i].getText())
-    
+
     # Concatinate all pieces of text in website_text into a single string
     # This will be used for the Long Audio File
     full_text = ''
@@ -34,17 +35,18 @@ def web_scraper(event, context):
 #        OutputFormat = "mp3",
 #        VoiceId="Joanna"
 #    )
-    
-    # Send mp3 to audio file    
+
+    # Send mp3 to audio file
 #    with closing(audio_response['AudioStream']) as stream:
 #        with open('audio_file.mp3', 'wb') as audio_file:
 #            audio_file.write(stream.read())
-    
+
     # Testing to see if a Long Audio File would be better
     bucket_name = event['bucket_name']
     long_audio = polly.start_speech_synthesis_task(
         OutputFormat = 'mp3',
         OutputS3BucketName = bucket_name,
+        OutputS3KeyPrefix = file_name,
         VoiceId = "Joanna",
         Text = full_text,
         TextType = 'text'
